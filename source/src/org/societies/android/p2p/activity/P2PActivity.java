8181b3125771a1bf8c2a0c17f9ba2cecad0ddfec
Kato Stølen <kato.stoelen@gmail.com>
2013-04-23 14:44:23 +0200

    Added activity for P2P.

---
+++ source/src/org/societies/android/p2p/activity/P2PActivity.java
@@ -0,0 +1,17 @@
+package org.societies.android.p2p.activity;
+
+import org.societies.android.platform.R;
+import org.societies.android.platform.R.layout;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class P2PActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_p2p);
+	}
+}
