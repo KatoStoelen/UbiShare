31ea15213054e212c7af6ff1b4ca87cdf148672a
Kato Stølen <kato.stoelen@gmail.com>
2013-04-25 13:34:55 +0200

    Added button to open the P2P activity in the main activity.

--- source/src/org/societies/android/platform/SocialProviderActivity.java
+++ source/src/org/societies/android/platform/SocialProviderActivity.java
@@ -1,7 +1,11 @@
 package org.societies.android.platform;
 
+import org.societies.android.p2p.activity.P2PActivity;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SocialProviderActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,14 @@ public class SocialProviderActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    /**
+     * On-click handler of the P2P button.
+     * @param view The clicked view.
+     */
+    public void onP2pButtonClick(View view) {
+    	Intent intent = new Intent(this, P2PActivity.class);
+    	startActivity(intent);
     }
 }
